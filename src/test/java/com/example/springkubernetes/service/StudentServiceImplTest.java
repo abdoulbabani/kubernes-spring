d7ef4762124c0204d3b6777bcdfeb0c084174205
abdoulkarim <a.garba@abalsia.com>
2022-09-02 22:43:42 +0200

    deployment-spring-kubernetes

---
+++ src/test/java/com/example/springkubernetes/service/StudentServiceImplTest.java
@@ -0,0 +1,24 @@
+package com.example.springkubernetes.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentServiceImplTest {
+
+    @Test
+    void getAllStudent() {
+    }
+
+    @Test
+    void getSudentByIde() {
+    }
+
+    @Test
+    void addStudent() {
+    }
+
+    @Test
+    void deletteStudentByide() {
+    }
+}
